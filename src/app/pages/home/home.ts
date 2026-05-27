def710dc3214dce1cc9722bb5a2f1687fd978092
Gustavo <Gustavohenriquelive12@gmail.com>
2026-05-27 13:04:55 -0300

    Criacao de animação expansivel na lista historico

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { PageForm } from '../../component/page-form/page-form';
-import { ListHistoricoPage } from '../../component/list-historico-pendentes/list-historico-page';
+import { ComponenteDeTestes } from '../../component/componente-de-testes/componente-de-testes';
 import { HistoricoOrdemServico } from '../../component/historico-ordem-servico/historico-ordem-servico';
 
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [HistoricoOrdemServico],
+  imports: [HistoricoOrdemServico, ComponenteDeTestes],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
