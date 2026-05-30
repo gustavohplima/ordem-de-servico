1d48b0593f072b8c1e5c9236678779fca964bf34
Gustavo <Gustavohenriquelive12@gmail.com>
2026-05-30 11:46:49 -0300

    Modificação na rota e adicionando funcionalidades na pagina de historico

---
+++ src/app/pages/historico/historico.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ComponenteDeTestes } from "../../component/componente-de-testes/componente-de-testes";
+import { HistoricoOrdemServico } from "../../component/historico-ordem-servico/historico-ordem-servico";
+
+@Component({
+  standalone: true,
+  selector: 'app-historico',
+  imports: [ComponenteDeTestes, HistoricoOrdemServico],
+  templateUrl: './historico.html',
+  styleUrl: './historico.css',
+})
+export class Historico {}
